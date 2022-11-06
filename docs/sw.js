e396db2b919969ecc264ca1b3d6cf69322be637f
Jakub Jankiewicz <jcubic@onet.pl>
2022-11-06 21:57:45 +0100

    update demo

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
-import { Wayne } from 'https://cdn.jsdelivr.net/gh/jcubic/wayne@master/index.js';
+importScripts('../index.umd.js');
 
-const app = new Wayne();
+const app = new wayne.Wayne();
 
 const users = {
   1: 'Jakub T. Jankiewicz',
@@ -19,4 +19,28 @@ app.get('/user/{id}', function(req, res) {
 
 app.get('/error', function(req, res) {
   nonExisting();
+});
+
+app.get('/redirect', function(req, res) {
+  res.redirect(301, '/message');
+});
+
+app.get('/message', function(req, res) {
+  res.text('Lorem Ipsum');
+});
+
+app.get('/external', function(req, res) {
+  res.redirect('https://api.buildable.dev/@62d55492951509001abc363e/live/lorem-ipsum');
+});
+
+app.get('/sse', function(req, res) {
+  const stream = res.sse({
+    onClose() {
+      clearInterval(timerId);
+    }
+  });
+  var timerId = setInterval(function() {
+    const now = (new Date()).toString();
+    stream.send({ data: now });
+  }, 1000);
 });
