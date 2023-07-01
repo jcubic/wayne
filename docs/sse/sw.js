fa07ab189df1372792f439764c1f1992095b21f7
Jakub Jankiewicz <jcubic@onet.pl>
2023-07-01 22:02:50 +0200

    add Server-Sent Events Proxy demo #17

---
+++ docs/sse/sw.js
@@ -0,0 +1,69 @@
+import { Wayne } from 'http://localhost/~kuba/jcubic/wayne/repo/index.js';
+
+const app = new Wayne();
+let stream;
+let hub;
+
+class EventEmitter {
+    constructor() {
+        this.handlers = {};
+    }
+    on(event, handler) {
+        if (!this.handlers[event]) {
+            this.handlers[event] = [];
+        }
+        this.handlers[event].push(handler);
+    }
+    emit(event, data) {
+        if (this.handlers[event]) {
+            this.handlers[event].forEach(handler => {
+                handler(data);
+            });
+        }
+    }
+    is_empty(event) {
+        return Boolean(this.handlers[event] && this.handlers[event].length);
+    }
+    off(event, handler = null) {
+        if (handler === null) {
+            delete this.handlers[event];
+        } else {
+            const handlers = this.handlers[event];
+            if (handlers) {
+                this.handlers[event] = handlers.filter(h => {
+                    return handler !== h;
+                });
+            }
+        }
+    }
+}
+
+
+app.get('/stream', (req, res) => {
+    console.log('Attempt');
+    if (!hub) {
+        init();
+    }
+    function handler(data) {
+        stream.send({ event: 'time', data });
+    }
+    const stream = res.sse({
+        onClose() {
+            hub.off('data', handler);
+            if (hub.is_empty('data')) {
+                stream.close();
+                hub = stream = null;
+            }
+        }
+    });
+    hub.on('data', handler);
+});
+
+function init() {
+    stream = new EventSource('https://jcubic.pl/stream.php');
+    hub = new EventEmitter();
+    stream.addEventListener('time', (event) => {
+        console.log('emit', event.data);
+        hub.emit('data', event.data);
+    });
+}
