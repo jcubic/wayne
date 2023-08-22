79584bea147491f3a8946620e57647dfd19cc270
Jakub Jankiewicz <jcubic@onet.pl>
2023-08-22 17:37:55 +0200

    add source code syntax highlighting demo

---
+++ docs/code/sw.js
@@ -0,0 +1,36 @@
+importScripts(
+  'https://cdn.jsdelivr.net/npm/@jcubic/wayne/index.umd.min.js',
+  'https://cdn.jsdelivr.net/npm/prismjs/prism.js',
+  'https://cdn.jsdelivr.net/gh/jcubic/static@master/js/path.js'
+);
+
+const app = new wayne.Wayne();
+
+const language_map = {
+  js: 'javascript',
+  css: 'css'
+};
+
+const style = '<link href="https://cdn.jsdelivr.net/npm/prismjs/themes/prism-coy.css" rel="stylesheet"/>';
+
+app.get('*', async function(req, res) {
+  const url = new URL(req.url);
+  const extension = path.extname(url.pathname).substring(1);
+  const language = language_map[extension];
+  const accept = req.headers.get('Accept');
+  if (language && Prism.languages[language] && accept.match(/text\/html/)) {
+      const code = await fetch(req.url).then(res => res.text());
+      const grammar = Prism.languages[language];
+      const tokens = Prism.tokenize(code, grammar);
+      const output = Prism.Token.stringify(tokens, language);
+      res.html(`${style}\n<pre><code class="language-${language}">${output}</code></pre>`);
+  } else {
+      const fetch_res = await fetch(req.url);
+      const type = fetch_res.headers.get('Content-Type');
+      res._resolve(fetch_res);
+  }
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
