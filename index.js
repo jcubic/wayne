f52289c79d19864f0abc3f990846f1b198c82091
Jakub Jankiewicz <jcubic@onet.pl>
2024-01-27 18:43:55 +0100

    update version

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*
- * Wayne - Server Worker Routing library (v. 0.12.0)
+ * Wayne - Server Worker Routing library (v. 0.13.0)
  *
  * Copyright (c) 2022-2024 Jakub T. Jankiewicz <https://jcubic.pl/me>
  * Released under MIT license
