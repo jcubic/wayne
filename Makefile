24df1e651e8de67b308c60179e915ed06c0f1733
Jakub Jankiewicz <jcubic@onet.pl>
2024-03-22 21:20:15 +0100

    version 0.16.0

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION=0.15.0
+VERSION=0.16.0
 DATE=`date -uR`
 YEAR=`date +%Y`
 
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*
- * Wayne - Server Worker Routing library (v. 0.15.0)
+ * Wayne - Server Worker Routing library (v. 0.16.0)
  *
  * Copyright (c) 2022-2024 Jakub T. Jankiewicz <https://jcubic.pl/me>
  * Released under MIT license
